fac0e653fca0a0b773b436aaf356fc09db62681c
Elod Gyorgy <elod.gyorgy@digilent.ro>
2020-08-19 15:02:37 +0300

    Set project board repo path to local.
    This base project is used to create the board definition files. Therefore it does not rely on external board repositories like the Vivado installation directory.

--- project_info.tcl
+++ project_info.tcl
@@ -1,6 +1,8 @@
 # This is an automatically generated file used by digilent_vivado_checkout.tcl to set project options
 proc set_project_properties_post_create_project {proj_name} {
     set project_obj [get_projects $proj_name]
+	global repo_path
+	set_propert BOARD_PART_REPO_PATHS [file normalize "$repo_path/board_files/"] $project_obj
 	set_property "part" "xczu5ev-sfvc784-1-e" $project_obj
 	set_property "board_part" "digilentinc.com:gzu_5ev:part0:1.0" $project_obj
 	set_property "default_lib" "xil_defaultlib" $project_obj
