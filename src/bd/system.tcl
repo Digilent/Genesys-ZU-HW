d5ee497425ddb9f181bc1032140c0e4e77138256
Elod Gyorgy <elod.gyorgy@digilent.ro>
2021-08-02 18:02:57 +0300

    Renamed block design for consistency with other apps.

--- src/bd/system.tcl
+++ src/bd/system.tcl
@@ -1,6 +1,6 @@
 
 ################################################################
-# This is a generated script based on design: design_1
+# This is a generated script based on design: system
 #
 # Though there are limitations about the generated script,
 # the main purpose of this utility is to make learning
@@ -35,7 +35,7 @@ if { [string first $scripts_vivado_version $current_vivado_version] == -1 } {
 ################################################################
 
 # To test this script, run the following commands from Vivado Tcl console:
-# source design_1_script.tcl
+# source system_script.tcl
 
 # If there is no project opened, this script will create a
 # project, but make sure you do not have an existing project
@@ -50,7 +50,7 @@ if { $list_projs eq "" } {
 
 # CHANGE DESIGN NAME HERE
 variable design_name
-set design_name design_1
+set design_name system
 
 # If you do not already have an existing IP Integrator design open,
 # you can create a design using the following command:
